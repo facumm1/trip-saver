5c5bcf4fc0d28e082b7553f57048b9ded33cab3f
Facundo <facundomamani120@gmail.com>
2023-05-18 16:15:15 -0300

    added firebase auth anonymously

--- src/TripSaver.js
+++ src/TripSaver.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator from './navigation/StackNavigator';
+import auth from '@react-native-firebase/auth';
 
 const TripSaver = () => {
+  useEffect(() => {
+    auth()
+      .signInAnonymously()
+      .then(() => {
+        //console.log('User signed in anonymously');
+      })
+      .catch(error => {
+        if (error.code === 'auth/operation-not-allowed') {
+          console.log('Enable anonymous in your firebase console.');
+        }
+
+        console.error(error);
+      });
+  }, []);
+
   return (
     <NavigationContainer>
       <StackNavigator />
